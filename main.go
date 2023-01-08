7dfcd9fad3f0c0909ed9f86a08814daa6d6b9630
Mai Mouri <sfyitan@gmail.com>
2023-01-08 23:59:10 +0900

    controller-modelsバージョン

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"strconv"
+	"app/controller"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -31,54 +31,6 @@ func dbInit() {
 	defer db.Close()
 }
 
-// DB追加
-func dbInsert(text string, status string) {
-	db, err := gorm.Open("mysql", "root:secret@tcp(db:3306)/sample?parseTime=true")
-	if err != nil {
-		panic("データベース開けず！（dbInsert)")
-	}
-	db.Create(&Todo{Text: text, Status: status})
-	defer db.Close()
-}
-
-// DB全取得
-func dbGetAll() []Todo {
-	db := GetDBConnection()
-	var todos []Todo
-	db.Order("created_at desc").Find(&todos)
-	db.Close()
-	return todos
-}
-
-// DB一つ取得
-func dbGetOne(id int) Todo {
-	db := GetDBConnection()
-	var todo Todo
-	db.First(&todo, id)
-	db.Close()
-	return todo
-}
-
-// DB更新
-func dbUpdate(id int, text string, status string) {
-	db := GetDBConnection()
-	var todo Todo
-	db.First(&todo, id)
-	todo.Text = text
-	todo.Status = status
-	db.Save(&todo)
-	db.Close()
-}
-
-// DB削除
-func dbDelete(id int) {
-	db := GetDBConnection()
-	var todo Todo
-	db.First(&todo, id)
-	db.Delete(&todo)
-	db.Close()
-}
-
 func GetDBConnection() *gorm.DB {
 	db, err := gorm.Open("mysql", "root:secret@tcp(db:3306)/sample?parseTime=true")
 
@@ -95,54 +47,19 @@ func main() {
 	dbInit()
 
 	//Index
-	r.GET("/", func(ctx *gin.Context) {
-		todos := dbGetAll()
-		ctx.HTML(200, "index.html", gin.H{
-			"todos": todos,
-		})
-	})
+	r.GET("/", controller.DbGetAll)
 
 	//Create
-	r.POST("/new", func(ctx *gin.Context) {
-		text := ctx.PostForm("text")
-		status := ctx.PostForm("status")
-		dbInsert(text, status)
-		ctx.Redirect(302, "/")
-	})
+	r.POST("/new", controller.DbInsert)
 
 	//Detail
-	r.GET("/detail/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic(err)
-		}
-		todo := dbGetOne(id)
-		ctx.HTML(200, "detail.html", gin.H{"todo": todo})
-	})
+	r.GET("/detail/:id", controller.DbGetOne)
 
 	//削除確認
-	r.GET("/delete_check/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		todo := dbGetOne(id)
-		ctx.HTML(200, "delete.html", gin.H{"todo": todo})
-	})
+	r.GET("/delete_check/:id", controller.DbDeleteCheck)
 
 	//Delete
-	r.POST("/delete/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		dbDelete(id)
-		ctx.Redirect(302, "/")
-
-	})
+	r.POST("/delete/:id", controller.DbDelete)
 
 	r.Run(":8080")
 }
